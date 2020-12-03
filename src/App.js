81279b2e30a16dde7bd11cc4aa4166dcd13801bd
sguan12 <stephanieguan12@gmail.com>
2020-12-03 12:44:23 -0800

    fixed DOM bug

--- src/App.js
+++ src/App.js
@@ -2,7 +2,17 @@ import React from 'react';
 
 function App() {
   return (
-    null
+    <div className='App'>
+      <div class="data">
+        <div>Sunday</div>
+        <div>Monday</div>
+        <div>Tuesday</div>
+        <div>Wednesday</div>  
+        <div>Thursday</div>
+        <div>Friday</div>
+        <div>Saturday</div>
+      </div>
+    </div>
   );
 }
 
