46c5716f0a496e6ab9493e885faf39b40903c877
Stephanie Guan <49424605+sguan12@users.noreply.github.com>
2021-01-07 21:53:05 -0800

    setting up json, css edits

--- src/App.js
+++ src/App.js
@@ -1,26 +1,41 @@
 import React from 'react';
 
 var key = "a8c4082333a7853f46479418b8da3462";
-
-function data(city) {
-  fetch('api.openweathermap.org/data/2.5/forecast?q=' + city + '&appid=' + key)  
-	.then(res => res.json())
-	// .then( ? );
-}
+const city = "Newark"
+fetch('https://api.openweathermap.org/data/2.5/forecast?q=' + city + '&cnt=5&units=imperial&appid=' + key)  
+.then(response => response.json())
+.then(data => console.log(data));
 
 function App() {
   return (
     <div className='App'>
-      <div class="title">
+
+      <div className="title">
         atmosphere
       </div>
-      <div class="data">
-        <div class="card">Monday</div>
-        <div class="card">Tuesday</div>
-        <div class="card">Wednesday</div>  
-        <div class="card">Thursday</div>
-        <div class="card">Friday</div>
+
+      {/* todo: make a search bar */}
+
+      <div className="data">
+        <div className="current">
+          <p>january 7</p>
+        </div>
+        <div className="other">
+          <div className="card">
+            <p>january 8</p>
+          </div>
+          <div className="card">
+            <p>january 9</p>
+          </div>
+          <div className="card">
+            <p>january 10</p>
+          </div>
+          <div className="card">
+            <p>january 11</p>
+          </div>
+        </div>
       </div>
+
     </div>
   );
 }
