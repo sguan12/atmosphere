1249ed9a2e2d5dc500aeb74cb75d7a7d86171747
Stephanie Guan <49424605+sguan12@users.noreply.github.com>
2021-01-09 18:20:14 -0800

    display api info

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
 import React from 'react';
 
 var key = "a8c4082333a7853f46479418b8da3462";
-const city = "Newark"
-fetch('https://api.openweathermap.org/data/2.5/forecast?q=' + city + '&cnt=5&units=imperial&appid=' + key)  
+const city = "San Francisco";
+
+// using current weather API to obtain longitude and latitude
+fetch('https://api.openweathermap.org/data/2.5/weather?q=' + city + '&units=imperial&appid=' + key)  
 .then(response => response.json())
-.then(data => console.log(data));
+.then(data1 => onecall(data1));
+
+function onecall(data1) {
+  var lon = data1.coord.lon;
+  var lat = data1.coord.lat;
+  // then fetching more data from one call weather API using lon and lat
+  fetch('https://api.openweathermap.org/data/2.5/onecall?lat=' + lat + '&lon=' + lon + '&units=imperial&appid=' + key)  
+  .then(response => response.json())
+  .then(data2 => console.log(data2));
+}
+
+function update(data2) {
+  // updating current weather card
+  var temp = Math.round(data2.current.temp);
+  var max = Math.round(data2.daily[0].temp.max);
+  var min = Math.round(data2.daily[0].temp.min);
+  document.getElementById('ctemp').innerHTML = temp;
+  document.getElementById('cm').innerHTML = max + ' / ' + min;
+}
 
 function App() {
   return (
     <div className='App'>
 
-      <div className="title">
-        atmosphere
+      <div className="info">
+        <p id="title"> atmosphere </p>
+        <p id="location"> Newark, California </p>
       </div>
-
       {/* todo: make a search bar */}
 
       <div className="data">
+
         <div className="current">
           <p>january 7</p>
+          <p id="ctemp">60</p>
+          <p id="cm">max and min</p>
         </div>
+
         <div className="other">
           <div className="card">
             <p>january 8</p>
@@ -34,6 +58,7 @@ function App() {
             <p>january 11</p>
           </div>
         </div>
+
       </div>
 
     </div>
