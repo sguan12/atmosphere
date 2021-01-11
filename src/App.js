7101b54db78d4e89607065aa3f368c44fe3f5f96
Stephanie Guan <49424605+sguan12@users.noreply.github.com>
2021-01-10 17:37:53 -0800

    set up search bar and units button

--- src/App.js
+++ src/App.js
@@ -2,10 +2,11 @@ import React from 'react';
 
 // using open weather map
 var apikey = "a8c4082333a7853f46479418b8da3462";
-const city = "San Francisco";
+var city = "San Francisco";
+var units = "imperial";
 
 // current weather API gives longitude and latitude
-fetch('https://api.openweathermap.org/data/2.5/weather?q=' + city + '&units=imperial&appid=' + apikey)  
+fetch('https://api.openweathermap.org/data/2.5/weather?q=' + city + '&units=' + units + '&appid=' + apikey)  
 .then(response => response.json())
 .then(data1 => onecall(data1));
 
@@ -14,7 +15,7 @@ function onecall(data1) {
   var lon = data1.coord.lon;
   var lat = data1.coord.lat;
   document.getElementById('location').innerHTML = city + ", " + data1.sys.country;
-  fetch('https://api.openweathermap.org/data/2.5/onecall?lat=' + lat + '&lon=' + lon + '&units=imperial&appid=' + apikey)  
+  fetch('https://api.openweathermap.org/data/2.5/onecall?lat=' + lat + '&lon=' + lon + '&units=' + units + '&appid=' + apikey)  
   .then(response => response.json())
   .then(data2 => update(data2));
 }
@@ -51,18 +52,36 @@ function update(data2) {
   document.getElementById('temp3').innerHTML = t3max + '° / ' + t3min + '°';
   document.getElementById('w4').innerHTML = w4;
   document.getElementById('temp4').innerHTML = t4max + '° / ' + t4min + '°';
+  // style adjustments
+  if (data2.current.dt > data2.current.sunset) night();
 }
 
+// change units
+function changeunits() {
+  if (units === 'imperial') {
+    units = 'metric';
+    document.getElementById('units').innerHTML = "use imperial units";
+    console.log("it works");
+  }
+  else {
+    units = 'imperial';
+    document.getElementById('units').innerHTML = "use metric units";
+  }
+  window.location.reload();
+}
 
-var d = new Date();
 // spelled out months for generating date
 const months = ["january", "february", "march", "april", "may", "june", "july", "august",
                 "september", "october", "november", "december"];
+// changing background color based on time
+function night() {
+  document.body.style = "background-image: linear-gradient(rgb(30, 47, 71), rgb(74, 95, 104));";
+}
 
 // haiku dictionary
 const haikus = {
   "clear": " warm, radiant sun <br> offers us a beaming smile <br> lights our world with joy ",
-  "clouds": " a shy ray of light <br> cautiously peeks through the clouds <br> stays partly hidden ",
+  "clouds": " a shy ray of light <br> cautiously peeks behind clouds <br> stays partly hidden ",
   "overcast clouds": " gray and moody skies <br> there's no sun to be seen but <br> good photo lighting ",
   "thunderstorm": " rain but with a flash <br> fierce lightning pierce the heavens <br> booming drums above ",
   "rain": " heavy raindrops fall <br> rest your eyes and listen close <br> nature's lullaby ",
@@ -99,7 +118,9 @@ function App() {
             <p id="ctemp">60°</p>
             <p id="cm">65° / 60°</p>
           </div>
+          <p id="haikutitle">today's weather:</p>
           <p id="haiku">the haiku goes here</p>
+          <button type="button" id="units" onclick={changeunits}>use metric units</button>
         </div>
 
         <div className="other">
